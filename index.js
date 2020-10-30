5fffe14ba147a8f39928549e0cf72fec2faf32fc
vineela96 <43242924+vineela96@users.noreply.github.com>
2020-10-30 13:52:37 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import ScotchInfoBar from "./ScotchInfoBar";
+
+function App() {
+  // MOST OF YOUR CODE GOES HERE
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  return (
+    <div className="App">
+      <div>
+        <h2 className="subtitle is-4">Update Data from an input</h2>
+      </div>
+
+      {/* Display Data */}
+      <div className="input-display">
+        <p>Display Name: {name} </p>
+        <p>Display Age: {age}</p>
+      </div>
+
+      {/* Collect User Inputs */}
+      <div className="inputs">
+        {/* Input name */}
+        <div className="field">
+          <label className="label">Name: </label>
+          <input className="input" type="text" placeholder="William" onChange = {e => setName(e.target.value)}/>
+        </div>
+
+        {/* Input age */}
+        <div className="field">
+          <label className="label">Age: </label>
+          <input className="input" type="number" placeholder="38" onChange = {e => setAge(e.target.value)} />
+        </div>
+      </div>
+      <ScotchInfoBar seriesNumber="3" />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
